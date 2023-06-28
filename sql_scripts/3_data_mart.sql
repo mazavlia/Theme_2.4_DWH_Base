03b0400502a34fba6f0e5f80baccb317da67e5aa
Marina Zaporozhets <mazavlia@mail.ru>
2023-06-29 01:17:11 +0500

    Changing datamart-script

--- sql_scripts/3_data_mart.sql
+++ sql_scripts/3_data_mart.sql
@@ -12,54 +12,23 @@ BEGIN
 - income_fact/income_plan — отношение фактического дохода к запланированному.';
 END;
 $$;
-WITH subquery_citilink AS (SELECT DATE_PART('MONTH',  sale_date) AS sale_month,
-	   							 shop_name, 
-	   							 product_name, 
-	   							 SUM(sales_cnt)  sales_fact,
-	   							 SUM(plan_cnt) sales_plan,
-	   							 ROUND(SUM(sales_cnt)::NUMERIC / SUM(plan_cnt), 2) "sales_fact/sales_plan",
-	   							 SUM(price * sales_cnt) income_fact, 
-	   							 SUM(price * plan_cnt) income_plan,
-	   							 ROUND((SUM(price * sales_cnt)::NUMERIC / SUM(price * plan_cnt)), 2) "income_fact/income_plan"
-						  FROM shops
-						  JOIN shop_citilink USING (shop_id)
-						  JOIN products USING (product_id)
-						  JOIN plan USING (product_id)
-						  WHERE DATE_PART('MONTH',  sale_date) = 5
-						  GROUP BY sale_month, shop_name, product_name),
-	 subquery_dns AS (SELECT DATE_PART('MONTH',  sale_date) AS sale_month,
-	   							 shop_name, 
-	   							 product_name, 
-	   							 SUM(sales_cnt)  sales_fact,
-	   							 SUM(plan_cnt) sales_plan,
-	   							 ROUND(SUM(sales_cnt)::NUMERIC / SUM(plan_cnt), 2) "sales_fact/sales_plan",
-	   							 SUM(price * sales_cnt) income_fact, 
-	   							 SUM(price * plan_cnt) income_plan,
-	   							 ROUND((SUM(price * sales_cnt)::NUMERIC / SUM(price * plan_cnt)), 2) "income_fact/income_plan"
-						  FROM shops
-						  JOIN shop_dns USING (shop_id)
-						  JOIN products USING (product_id)
-						  JOIN plan USING (product_id)
-						  WHERE DATE_PART('MONTH',  sale_date) = 5
-						  GROUP BY sale_month, shop_name, product_name),
-	 subquery_mvideo AS (SELECT DATE_PART('MONTH',  sale_date) AS sale_month,
-	   							 shop_name, 
-	   							 product_name, 
-	   							 SUM(sales_cnt)  sales_fact,
-	   							 SUM(plan_cnt) sales_plan,
-	   							 ROUND(SUM(sales_cnt)::NUMERIC / SUM(plan_cnt), 2) "sales_fact/sales_plan",
-	   							 SUM(price * sales_cnt) income_fact, 
-	   							 SUM(price * plan_cnt) income_plan,
-	   							 ROUND((SUM(price * sales_cnt)::NUMERIC / SUM(price * plan_cnt)), 2) "income_fact/income_plan"
-						  FROM shops
-						  JOIN shop_mvideo USING (shop_id)
-						  JOIN products USING (product_id)
-						  JOIN plan USING (product_id)
-						  WHERE DATE_PART('MONTH',  sale_date) = 5
-						  GROUP BY sale_month, shop_name, product_name)
-SELECT * FROM subquery_citilink
-UNION ALL 
-SELECT * FROM subquery_dns
-UNION ALL 
-SELECT * FROM subquery_mvideo
+WITH union_shops_tabl AS (SELECT * FROM shop_citilink
+						  UNION SELECT * FROM shop_dns
+						  UNION SELECT * FROM shop_mvideo
+						  ORDER BY sale_date, shop_id, product_id)	  
+SELECT DATE_PART('MONTH',  sale_date) AS sale_month,
+	   shop_name, 
+	   product_name, 
+	   SUM(sales_cnt)  sales_fact,
+	   SUM(plan_cnt) sales_plan,
+	   ROUND(SUM(sales_cnt)::NUMERIC / SUM(plan_cnt), 2) "sales_fact/sales_plan",
+	   SUM(price * sales_cnt) income_fact, 
+	   SUM(price * plan_cnt) income_plan,
+	   ROUND((SUM(price * sales_cnt)::NUMERIC / SUM(price * plan_cnt)), 2) "income_fact/income_plan"
+FROM shops
+JOIN union_shops_tabl USING (shop_id)
+JOIN products USING (product_id)
+JOIN plan USING (product_id)
+WHERE DATE_PART('MONTH',  sale_date) = 5
+GROUP BY sale_month, shop_name, product_name
 ORDER BY shop_name, product_name
